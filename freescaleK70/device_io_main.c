9ab6865312386ac2b8932688fbec5f6292853b6c
Stephen Karger <stephen.karger@gmail.com>
2014-04-27 19:32:19 -0400

    enhance device_io_main with file system demo

--- freescaleK70/device_io_main.c
+++ freescaleK70/device_io_main.c
@@ -1,19 +1,25 @@
-#include "derivative.h" /* include peripheral declarations */
 
+#include "../include/constants.h"
+#include "io_constants.h"
 #include "io.h"
 #include "hardware/delay.h"
+#include "../memory/memory.h"
+
 #include <stdio.h>
 
 void fputc_led_demo(char *led) {
     Stream *stream;
     stream = myFopen(led);
+    printf("Turn on %s\n", led);
     myFputc(1, stream);
     delay(1000000);
+    printf("Turn off %s\n", led);
     myFputc(0, stream);
     myFclose(stream);
 }
 
 void fgetc_button_demo(void) {
+    printf("A total of 6 button presses will terminate program\n");
     Stream *stream1 = myFopen("/dev/button/sw1");
     Stream *stream2 = myFopen("/dev/button/sw2");
     int counter = 0;
@@ -37,14 +43,56 @@ void fgetc_button_demo(void) {
     }
 }
 
+void fs_demo(void) {
+    Stream *stream;
+    int a, b, c;
+
+    printf("Create /dev/fs/text...\n");
+    myCreate("/dev/fs/text");
+
+    printf("Open a stream on /dev/fs/text...\n");
+    stream = myFopen("/dev/fs/text");
+
+    printf("Write characters a, b, c to the stream...\n");
+    myFputc('a', stream);
+    myFputc('b', stream);
+    myFputc('c', stream);
+
+    printf("Read a character and print it\n");
+    a = myFgetc(stream);
+    printf("%c\n", a);
+
+    printf("Read a character and print it\n");
+    b = myFgetc(stream);
+    printf("%c\n", b);
+
+    printf("Read a character and print it\n");
+    c = myFgetc(stream);
+    printf("%c\n", c);
+
+    printf("Close the stream\n");
+    myFclose(stream);
+
+    printf("Delete /dev/fs/text\n");
+    myDelete("/dev/fs/text");
+}
+
 int main(void) {
+    initialize_memory();
     initialize_io();
+
+    printf("In memory file-system:\n");
+    fs_demo();
+
+    printf("LEDs:\n");
     fputc_led_demo("/dev/led/orange");
     fputc_led_demo("/dev/led/yellow");
     fputc_led_demo("/dev/led/green");
     fputc_led_demo("/dev/led/blue");
 
+    printf("Buttons:\n");
     fgetc_button_demo();
 
+    printf("Goodbye\n");
     return 0;
 }
