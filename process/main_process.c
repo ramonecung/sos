0e981d038d0706662616f02406a14c3385307254
Stephen Karger <stephen.karger@gmail.com>
2014-06-27 21:15:32 -0400

    add slash

--- process/main_process.c
+++ process/main_process.c
@@ -61,7 +61,7 @@ int openFiles(int argc, char **argv) {
     svc_myFflush(STDOUT);
     svc_myFopen("/dev/led/blue");
     svc_myFopen("/dev/button/sw1");
-    svc_myFopen("dev/fs/data");
+    svc_myFopen("/dev/fs/data");
     /* kill me */
     svc_myKill(pid);
     return 0;
